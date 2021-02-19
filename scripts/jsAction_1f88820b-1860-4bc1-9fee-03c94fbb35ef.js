76227e9e21ef3e8965204a4463b8c33cd83fb04a
Quentin Dusser <qdusser@NX-DOMAIN.LAN>
2021-02-19 16:16:05 +0100

    hello

---
+++ scripts/jsAction_1f88820b-1860-4bc1-9fee-03c94fbb35ef.js
@@ -0,0 +1,9 @@
+﻿// Javascript skeleton.
+// Edit and adapt to your needs.
+// The documentation of the NeoLoad Javascript API
+// is available in the appendix of the documentation.
+
+// Get variable value from VariableManager
+var stock = context.variableManager.getValue("stock");
+
+logger.debug("le stock pour cet item est de : "+stock)
---
+++ scripts/jsAction_c86e4a0b-836d-427e-b5e8-9e6944051375.js
@@ -0,0 +1,9 @@
+﻿var alexis = context.variableManager.getValue("item");
+var time = context.variableManager.getValue("time");
+if (alexis==null || time==null) {
+context.fail("Variable not found");
+}
+
+logger.debug("The item is: "+alexis);
+logger.debug(" The time is: "+time);
+logger.debug(" The LG is: "+context.currentLG.name);
